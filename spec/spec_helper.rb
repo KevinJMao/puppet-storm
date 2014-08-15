e1a8025d012ac8c4c41c4b0740c9e5714722b1c7
Kevin Mao <kmao@verisign.com>
2014-08-15 13:43:47 -0400

    Added UTF_8 encoding fix to prevent unit test failure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,8 @@ RSpec.configure do |c|
     Puppet.features.stubs(:root? => true)
   end
 end
+
+if RUBY_VERSION =~ /1.9/
+    Encoding.default_external = Encoding::UTF_8
+    Encoding.default_internal = Encoding::UTF_8
+end
